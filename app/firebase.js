f0d7c68e4c3a349af94d619aa217a99e3a274eae
Daniel Cho <daniel.h.cho@student.unsw.edu.au>
2024-01-27 11:43:32 +1100

    modified env variables

--- app/firebase.js
+++ app/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_APP_MESSAGING_SENDER,
-  appId: process.env.NEXT_APP_APP_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
