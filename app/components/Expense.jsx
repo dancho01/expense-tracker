aeb620b00276fc7740e7e31f89309a481a0392f7
Daniel Cho <daniel.h.cho@student.unsw.edu.au>
2024-01-27 11:01:06 +1100

    added dropdown for category

--- app/components/Expense.jsx
+++ app/components/Expense.jsx
@@ -12,6 +12,7 @@ const Expense = () => {
   const [cost, setCost] = useState('')
   const [total, setTotal] = useState(0)
   const [user, setUser] = useState('')
+  const [category, setCategory] = useState('groceries')
 
   const userEmail = doc(db, 'users',`${user?.email}`)
 
@@ -26,6 +27,7 @@ const Expense = () => {
         items: arrayUnion({
           name: item,
           price: cost,
+          category: category,
         })
       })
     } catch (error) {
@@ -44,10 +46,10 @@ const Expense = () => {
   // read data
   useEffect(() => {
     onSnapshot(userEmail, (doc) => {
-      setItems(doc.data()?.items);
+      setItems(doc.data()?.items);  // reads items into array
       const totalPrice = doc.data()?.items.reduce(
         (sum, item) => sum + parseFloat(item.price), 0);
-      setTotal(totalPrice);
+      setTotal(totalPrice); // calculate total price from the items
     });
   }, [user?.email])
 
@@ -64,27 +66,46 @@ const Expense = () => {
       <h1 className='text-4xl p-16 text-center font-semibold'>Expense Tracker</h1>
       <div className='w-[800px] flex flex-col items-center bg-slate-800 rounded-md'>
         <div className='p-8'>
-          <form onSubmit={handleClick} className='flex gap-8'>
+          <form onSubmit={handleClick} className='grid grid-cols-2 gap-8 items-center'>
             <div className='text-black'>
-              <input value={item} onChange={(e) => setItem(e.target.value)} className='px-4 py-2' type="text" placeholder='Enter item' />
+              <input value={item} onChange={(e) => setItem(e.target.value)} className='px-4 py-2' autoFocus type="text" placeholder='Enter item' />
             </div>
             <div className='text-black'>
               <input value={cost} onChange={(e) => setCost(e.target.value)} className='px-4 py-2 ' type="text" placeholder='Enter $' />
             </div>
-            <button className='border border-slate-900 bg-slate-900 px-2 py-2 hover:bg-slate-800'>Add item</button>
+            <div>
+            <label className='text-lg'>Category: </label>
+              <select 
+                className='text-black' 
+                onChange={(e) => setCategory(e.target.value)}>
+                <option value={"groceries"} >Groceries</option>
+                <option value={'clothes'} >Clothes</option>
+                <option value={'utilities'} >Utilities</option>
+                <option value={'entertainment'} >Entertainment</option>
+                <option value={'other'}>Other</option>
+              </select>
+            </div>
+            <button className='border border-slate-900 bg-slate-900 px-2 py-2 hover:bg-slate-800 rounded-md'>Add item</button>
           </form>
         </div>
 
         {items?.map((currItem, index) => (
-          <div key={index} className='flex justify-between items-center w-[90%] bg-slate-900 px-8 py-2 my-2 rounded-md'>
-            <div className='flex justify-between w-[90%] '>
-              <div className='capitalize'>{currItem.name}</div>
-              <div>${currItem.price}</div>
+         
+          <div key={index} className={currItem.category === 'groceries' ? "flex justify-between items-center w-[90%] bg-green-700 px-8 py-2 my-2 rounded-md"
+                : currItem.category === 'clothes' ? 'flex justify-between items-center w-[90%] bg-purple-700 px-8 py-2 my-2 rounded-md'
+                : currItem.category === 'utilities' ? 'flex justify-between items-center w-[90%] bg-blue-700 px-8 py-2 my-2 rounded-md'
+                : currItem.category === 'entertainment' ? 'flex justify-between items-center w-[90%] bg-red-700 px-8 py-2 my-2 rounded-md'
+                : 'flex justify-between items-center w-[90%] bg-yellow-700 px-8 py-2 my-2 rounded-md'}>
+            <div className='grid grid-cols-3 w-full'>
+              <div className='capitalize font-semibold'>{currItem.name} </div> 
+              <div className='capitalize text-sm'>({currItem.category})</div>
+              <div className='font-semibold'>${currItem.price}</div>
             </div >
             <RiCloseCircleLine size={20} onClick={() => deleteItem(index)} className='cursor-pointer' />
           </div>
+  
         ))}
-        <div className='w-[75%] flex justify-end px-8 py-2 my-2'>
+        <div className='w-[40%] flex justify-end px-8 py-2 my-2'>
             <div>Total Cost:&emsp;${total}</div>
         </div>
 
